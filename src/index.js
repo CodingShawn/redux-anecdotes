1c955cfb6e6411f5cfccb9c6b00d0f2dce857ade
Shawn <codingshawn@gmail.com>
2021-04-12 22:50:36 +0800

    Added redux devtools as dependency and move store to its own file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
 
-const store = createStore(reducer)
+const reduxStore = store;
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={reduxStore}>
     <App />
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
