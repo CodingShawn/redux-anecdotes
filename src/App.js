9cc533dd5e4cb1ced2faf808be16304b5e548084
Shawn <codingshawn@gmail.com>
2021-04-14 22:51:17 +0800

    Implement functionality to retrieve anecdotes from json-server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { initialiseAnecdotes } from "./reducers/anecdoteReducer";
+import noteService from "./services/anecdotes";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then((anecdotes) => dispatch(initialiseAnecdotes(anecdotes)));
+  }, []);
+
   return (
     <div>
-      <Notification/>
-      <Filter/>
+      <Notification />
+      <Filter />
       <h2>Anecdotes</h2>
       <AnecdoteList />
       <AnecdoteForm />
